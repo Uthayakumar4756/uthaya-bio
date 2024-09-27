bb21e54d43768173c7a4c0f79f1e634b15bebac1
uthayakumar <111730347+Uthayakumar4756@users.noreply.github.com>
2024-09-27 11:43:24 +0530

    Update footer.js
    url change

--- assets/ariztidFooter/footer.js
+++ assets/ariztidFooter/footer.js
@@ -8,7 +8,7 @@ margin-top: 342px;
     <!-- Copyright -->
     <div class="text-center p-4" style=" background-color: rgb(250 250 250);margin-top: 41px;position: absolute;width: 100%; padding: 0.5rem !important;">
       © 2024 Copyright:
-      <a class="text-reset fw-bold" href="https://uthayakumar4756.github.io/uthaya-bio/userLogin.html">Ariztid.com</a>
+      <a class="text-reset fw-bold" href="https://uthayakumar4756.github.io/uthaya-bio/">Ariztid.com</a>
     </div>
     <!-- Copyright -->
   </footer>
