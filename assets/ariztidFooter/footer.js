ae6f8379c546f577208e65444292b252f09772ac
Your Name <you@example.com>
2024-09-27 16:52:50 +0530

    foot change commit

--- assets/ariztidFooter/footer.js
+++ assets/ariztidFooter/footer.js
@@ -12,11 +12,7 @@ margin-top: 342px;
     padding: 0.01rem !important;
     bottom: 0px;">
       © 2024 Copyright:
-<<<<<<< HEAD
       <a class="text-reset fw-bold" href="https://uthayakumar4756.github.io/uthaya-bio/">UthayaBio.com</a>
-=======
-      <a class="text-reset fw-bold" href="https://uthayakumar4756.github.io/uthaya-bio/">Ariztid.com</a>
->>>>>>> bb21e54d43768173c7a4c0f79f1e634b15bebac1
     </div>
     <!-- Copyright -->
   </footer>
